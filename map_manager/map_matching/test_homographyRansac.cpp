a56e05eb67b5cf4ee99b6a5c26696263d575a4a4
Martina <deturres@informatik.uni-freiburg.de>
2014-08-29 17:53:34 +0200

    asked tayyab to commit

--- map_manager/map_matching/test_homographyRansac.cpp
+++ map_manager/map_matching/test_homographyRansac.cpp
@@ -42,74 +42,74 @@ bool read_file(std::ifstream& is, Keypoints& k)
 }
 
 //compute the Homography and find the perspective transformed points with it
-void findHomography(Correspondances& good_matches, Mat H, Keypoints k_odomToMap) {
+void findHomographyM(Correspondances& good_matches, Mat& H, Keypoints& k_mapToOdom) {
 
-    Keypoints k_odom, k_map;
+    Keypoints k_map, k_odom;
     for(size_t i = 0; i < good_matches.first.size(); i++) {
 
         //get the keypoints from teh good matches
-        k_odom = good_matches.first;
-        k_map = good_matches.second;
+        k_map = good_matches.first;
+        k_odom = good_matches.second;
 
         // debug
-        cout << k_odom[i] << " <--> " << k_map[i] << endl;
+        cout << k_map[i] << " <--> " << k_odom[i] << endl;
     }
-    H = findHomography(k_odom, k_map, RANSAC);
+    H = findHomography(k_map, k_odom, RANSAC);
 
     // debug
     cout << "Homography H = "<< endl << " "  << H << endl << endl;
-    perspectiveTransform( k_odom, k_odomToMap, H);
+    perspectiveTransform(k_map, k_mapToOdom, H);
 
     // (visual)debug
-    ofstream os("odomtoMap.dat");
-    for (size_t i = 0; i < k_odomToMap.size(); i++)
+    ofstream os("mapToOdom.dat");
+    for (size_t i = 0; i < k_mapToOdom.size(); i++)
     {
-        cout << "Keypoints odom perspective transformed on the map\n" << k_odomToMap[i] << endl;
-        Vector2f odomToMap(Eigen::Vector2f(k_odomToMap[i].x, k_odomToMap[i].y));
-        os << odomToMap.transpose() << endl;
+        cout << "Keypoints map perspective transformed on the odom\n" << k_mapToOdom[i] << endl;
+        Vector2f mapToOdom(Eigen::Vector2f(k_mapToOdom[i].x, k_mapToOdom[i].y));
+        os << mapToOdom.transpose() << endl;
     }
 
-    /// decomposeHomography() is not working !!!!!!!!!!!!!!!
-    //svd(H) to find Scale, Rotation angle and Translation
-    //normalization
-    int a = sgn(H.at<double>(2,2));
-    double b = sqrt(pow(H.at<double>(2,0),2) + pow(H.at<double>(2,1), 2) + pow(H.at<double>(2,2),2));
-    double eta = a/b;
-    H = eta*H;
-//    cout << "eta = " << endl << " "  << eta << endl << endl;
-//    cout << "eta*H = " << endl << " "  << H << endl << endl;
+//    /// decomposeHomography() is not working !!!!!!!!!!!!!!!
+//    //svd(H) to find Scale, Rotation angle and Translation
+//    //normalization
+//    int a = sgn(H.at<double>(2,2));
+//    double b = sqrt(pow(H.at<double>(2,0),2) + pow(H.at<double>(2,1), 2) + pow(H.at<double>(2,2),2));
+//    double eta = a/b;
+//    H = eta*H;
+////    cout << "eta = " << endl << " "  << eta << endl << endl;
+////    cout << "eta*H = " << endl << " "  << H << endl << endl;
 
-    //extracting P pure projection matrix
-    Mat P = H.clone();
-    Mat I = Mat::eye(2,3,P.type());
-    I.copyTo(P.colRange(0,3).rowRange(0,2));
-//    cout << " P " << endl << " "  << P << endl << endl;
+//    //extracting P pure projection matrix
+//    Mat P = H.clone();
+//    Mat I = Mat::eye(2,3,P.type());
+//    I.copyTo(P.colRange(0,3).rowRange(0,2));
+////    cout << " P " << endl << " "  << P << endl << endl;
 
-    // H affine = H*P^-1
-    Mat Ha =  H*P.inv();
-    cout << " Ha " << endl << " "  << Ha << endl << endl;
+//    // H affine = H*P^-1
+//    Mat Ha =  H*P.inv();
+//    cout << " Ha " << endl << " "  << Ha << endl << endl;
 
-    //svd decomposition
-    SVD svd(Ha);
-    //extracting scale
-    Mat S = Mat::diag(svd.w);
-    //extracting theta
-    Mat R = svd.u*svd.vt.t();
-    double theta = atan2(R.at<double>(1,0),R.at<double>(0,0));
-    //extracting traslation
-    Vector3d t = Eigen::Vector3d(Ha.at<double>(2,0),Ha.at<double>(2,1),1);
+//    //svd decomposition
+//    SVD svd(Ha);
+//    //extracting scale
+//    Mat S = Mat::diag(svd.w);
+//    //extracting theta
+//    Mat R = svd.u*svd.vt.t();
+//    double theta = atan2(R.at<double>(1,0),R.at<double>(0,0));
+//    //extracting traslation
+//    Vector3d t = Eigen::Vector3d(Ha.at<double>(2,0),Ha.at<double>(2,1),1);
 
-    cout << "S = " << endl << " "  << S << endl << endl;
-    cout << "R = " << endl << " "  << R <<  ", with theta: "<< theta << endl << endl;
-    cout << "t = " << endl << " "  << t << endl << endl;
+//    cout << "S = " << endl << " "  << S << endl << endl;
+//    cout << "R = " << endl << " "  << R <<  ", with theta: "<< theta << endl << endl;
+//    cout << "t = " << endl << " "  << t << endl << endl;
 
 }
 
-void decomposeHomography(Mat& H, Mat S, Mat R, Vector3d t){
+void decomposeHomography(Mat& H, Mat& S, Mat& R, Vector3d& t){
 
     //normalization
     int a = sgn(H.at<double>(2,2));
-    double b = sqrt(pow(H.at<double>(2,0),2) + pow(H.at<double>(2,1), 2) + pow(H.at<double>(2,2),2));
+    double b = sqrt(pow(H.at<double>(2,0),2) + pow(H.at<double>(2,1), 2) + pow(H.at<double>(2,2),2)); //norm !!!!
     double eta = a/b;
     H = eta*H;
 //    cout << "eta = " << endl << " "  << eta << endl << endl;
@@ -125,15 +125,17 @@ void decomposeHomography(Mat& H, Mat S, Mat R, Vector3d t){
     Mat Ha =  H*P.inv();
     cout << " Ha " << endl << " "  << Ha << endl << endl;
 
-    //svd decomposition
+    //extracting traslation
+    double fuck = Ha.at<double>(2,0);
+    cout << fuck << endl;
+    t = Eigen::Vector3d(Ha.at<double>(0,2),Ha.at<double>(1,2),1);
+    //svd decomposition to extract R
     SVD svd(Ha);
     //extracting scale
     S = Mat::diag(svd.w);
     //extracting theta
     R = svd.u*svd.vt.t();
     double theta = atan2(R.at<double>(1,0),R.at<double>(0,0));
-    //extracting traslation
-    t = Eigen::Vector3d(Ha.at<double>(2,0),Ha.at<double>(2,1),1);
 
 //    cout << "S = " << endl << " "  << S << endl << endl;
 //    cout << "R = " << endl << " "  << R <<  ", with theta: "<< theta << endl << endl;
@@ -147,6 +149,15 @@ void decomposeHomography(Mat& H, Mat S, Mat R, Vector3d t){
 //    }
 }
 
+void constraintDefinition(Correspondances& good_matches, Mat& S, Mat& R, Vector3d& t) {
+    //creating constraints between features points
+    // f(0) = (0,0);
+    // f(x) = S * R * [u,v]_x - [u,v]_0 :: per ogni X
+
+
+
+}
+
 
 /**
  * @brief Main function
@@ -158,41 +169,37 @@ int main( int argc, char** argv )
         return -1;
     }
     //    cout << argv[1] << ' ' << argv[2] << endl;
-    ifstream feas_odom;
     ifstream feas_map;
-    feas_odom.open(argv[1]);
-    feas_map.open(argv[2]);
+    ifstream feas_odom;
+    feas_map.open(argv[1]);
+    feas_odom.open(argv[2]);
 
     // creating the good matches
-    Keypoints keypoints_odom;
     Keypoints keypoints_map;
+    Keypoints keypoints_odom;
 
     Correspondances good_matches;
     //reading from the file txt and creating the correspondances vector
-    read_file(feas_odom,keypoints_odom);
     read_file(feas_map,keypoints_map);
+    read_file(feas_odom,keypoints_odom);
 
     //for now manual good matches---> automatic find good correspondances needed..
-    good_matches = make_pair(keypoints_odom, keypoints_map);
+    good_matches = make_pair(keypoints_map, keypoints_odom);
     Mat H;
-    Keypoints k_odomToMap;
-    //find
-    findHomography(good_matches, H, k_odomToMap);
-
+    Keypoints k_mapToOdom;
+    //find homography
+    findHomographyM(good_matches, H, k_mapToOdom);
     cout << "Homography H = "<< endl << " "  << H << endl << endl;
 
-    //decompose
-//    Mat S,R;
-//    Vector3d t;
-//    decomposeHomography(H, S, R, t);
-//    cout << "S = " << endl << " "  << S << endl << endl;
-//    cout << "R = " << endl << " "  << R <<  ", with theta: "<< theta << endl << endl;
-//    cout << "t = " << endl << " "  << t << endl << endl;
-//    double theta = atan2(R.at<double>(1,0),R.at<double>(0,0));
+    //decompose homography
+    Mat S,R;
+    Vector3d t;
+    decomposeHomography(H, S, R, t);
+    cout << "S = " << endl << " "  << S << endl << endl;
+    cout << "R = " << endl << " "  << R << endl << endl;
+    cout << "t = " << endl << " "  << t << endl << endl;
 
-
-    //creating constraints between features points
-
+    constraintDefinition(good_matches, S, R, t);
 
     return 0;
 }
